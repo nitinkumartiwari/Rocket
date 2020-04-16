f1776e4dedd8a34720b89b40fe3815dfce7a43e3
nitinkumartiwari <nitin.nitesh38@gmail.com>
2020-04-16 18:10:21 +0530

    first commit with all features

---
+++ app/livechat/client/views/regular.js
@@ -0,0 +1,14 @@
+import './app/livechatAutocompleteUser';
+import './app/livechatQueue';
+import './app/livechatReadOnly';
+import './app/livechatNotSubscribed.html';
+import './app/livechatRoomTagSelector';
+import './app/tabbar/agentEdit';
+import './app/tabbar/agentInfo';
+import './app/tabbar/externalSearch';
+import './app/tabbar/visitorEdit';
+import './app/tabbar/visitorForward';
+import './app/tabbar/customerChatHistory';
+import './app/tabbar/customerChatHistoryMessages';
+import './app/tabbar/visitorInfo';
+import './app/tabbar/visitorNavigation';
